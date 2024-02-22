aba2d3ba48932ab81c35ba87cfb56065d93c9214
Luskar <144641549+LuskarJs@users.noreply.github.com>
2024-02-22 18:30:15 -0300

    Update loginPage.js

--- src/parts/login/loginPage.js
+++ src/parts/login/loginPage.js
@@ -15,7 +15,7 @@ const Login = () => {
         e.preventDefault();
     
         try {
-            const response = await fetch('http://depositodofabinho.com/login', {
+            const response = await fetch('https://depositodofabinho.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
